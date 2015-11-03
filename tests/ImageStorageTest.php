b7f9c85c1eaf724b8b9b9ba4ece8669cb129c588
Richard Marbach <rmarbach@gmail.com>
2015-11-03 23:20:40 +0100

    Added virtaul file system for testing

---
+++ tests/ImageStorageTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ImageStorageTest extends TestCase
+{
+   public function testImageStore()
+   {
+    
+   } 
+}
