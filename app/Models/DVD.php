af330d8cf8c246f7a388484aed9139b6b0581c97
Richard Marbach <rmarbach@gmail.com>
2015-11-05 14:20:23 +0100

    DVD and DVDInfo table now bound to correct tables

--- app/Models/DVD.php
+++ app/Models/DVD.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class DVD extends Model
 {
-    //
+    protected $table = 'dvds';
 }
--- app/Models/DVDInfo.php
+++ app/Models/DVDInfo.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class DVDInfo extends Model
 {
-    //
+  protected $table = 'dvd_info';
 }
