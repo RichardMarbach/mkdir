48cb1321d597d42458819386e64a7d5d44a616b1
Richard Marbach <rmarbach@gmail.com>
2015-11-10 15:55:33 +0100

    Added relationships to dvd_info table

--- app/Models/Producer.php
+++ app/Models/Producer.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Producer extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    /**
+     * @return mixed
+     */
+    public function dvds() {
+      return $this->belongsToMany('App\Models\DVDInfo', 'producer_dvd_info'
+          , 'producer_id', 'dvd_info_id')->withTimestamps();
+    }
+
 }
