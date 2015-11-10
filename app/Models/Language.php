1a9e55f86580be51665e8b8c713803b95341045d
Richard Marbach <rmarbach@gmail.com>
2015-11-10 16:12:19 +0100

    DVDs retrievable by language through language model

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Language extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    /**
+     * @return mixed
+     */
+    public function dvdsInLanguage() {
+      return $this->belongsToMany('App\Models\DVD', 'dvd_languages', 'language_id', 'dvd_id')
+          ->withTimestamps();
+    }
+
+    /**
+     * @return  mixed
+     */
+    public function dvdsWithSubtitles() {
+      return $this->belongsToMany('App\Models\DVD', 'dvd_subtitles', 'language_id', 'dvd_id')
+          ->withTimestamps();
+    }
 }
