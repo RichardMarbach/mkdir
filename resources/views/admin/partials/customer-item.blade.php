a022fa0cd2ca2aecee1b045a53ee91be3e607ac9
Richard Marbach <rmarbach@gmail.com>
2015-11-23 11:20:19 +0100

    Work on customer edit modal

--- resources/views/admin/partials/customer-item.blade.php
+++ resources/views/admin/partials/customer-item.blade.php
@@ -1,11 +1,15 @@
 <div class="col-sm-5">
-  <div class="thumbnail" id="{{$customer->id}}">
-    <h4 data-> {{ $customer->name }} </h4>
-    <h5>Email: <span>{{ $customer->user ? $customer->user->email : '' }}</span></h5>
-    <h5>Address: <span>{{ $customer->address }}</span></h5>
-    <h5>Phone: <span>{{ $customer->phone_number }}</span></h5>
-    <h5>Points: <span>{{ $customer->points }}</span></h5>
-    <h5>Birthdate: <span>{{ Helper::prettifyDate($customer->user ? $customer->user->birthdate : '') }}</span></h5>
+  <div class="thumbnail" data-id="{{$customer->id}}">
+    <h4 data-name="{{ $customer->name }}"> {{ $customer->name }} </h4>
+    <h5 data-email="{{ $customer->user ? $customer->user->email : '' }}">Email: 
+      <span>{{ $customer->user ? $customer->user->email : '' }}</span>
+    </h5>
+    <h5 data-address="{{ $customer->address }}">Address: <span>{{ $customer->address }}</span></h5>
+    <h5 data-phone="{{ $customer->phone_number }}">Phone: <span>{{ $customer->phone_number }}</span></h5>
+    <h5 data-points="{{ $customer->points }}">Points: <span>{{ $customer->points }}</span></h5>
+    <h5>Birthdate: 
+      <span>{{ Helper::prettifyDate($customer->user ? $customer->user->birthdate : '') }}</span>
+    </h5>
     <h5 data-admin="{{ $customer->user ? Helper::isAdmin($customer->user->roles) : 0 }}">
       Roles: 
       <span>
