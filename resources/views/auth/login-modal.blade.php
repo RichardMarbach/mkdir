7ac2ba3d1d59804e77eb586acd11fa2f5f5d7fb9
TAEJUNG KIM <tamedaslan@gmail.com>
2015-11-24 15:29:12 +0200

    ..

--- resources/views/auth/login-modal.blade.php
+++ resources/views/auth/login-modal.blade.php
@@ -32,13 +32,8 @@
         <span class="glyphicon glyphicon-remove"></span> Cancel
       </button>
       <p>Not a member? <a href="{{ url('register') }}">Sign Up</a></p>
-
     </div>
 
   </div>
 
   </div>
-</div>
-<div>
-    @include('common.errors')
-</div>
