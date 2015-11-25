a2673090331f29bfd7eb4617842f216bd157f66a
TAEJUNG KIM <tamedaslan@gmail.com>
2015-11-25 20:21:04 +0200

    commit
    inputtype

--- resources/views/auth/login-modal.blade.php
+++ resources/views/auth/login-modal.blade.php
@@ -14,7 +14,7 @@
         </div>
         <div class="form-group">
           <label for="psw"><span class="glyphicon glyphicon-lock"></span> Password</label>
-          <input type="text" class="form-control" id="psw" placeholder="Enter password">
+          <input type="password" class="form-control" id="psw" placeholder="Enter password">
         </div>
         <div class="checkbox">
           <label><input type="checkbox" value="" checked>Remember me</label>
@@ -24,9 +24,15 @@
     </div>
     <div class="modal-footer">
       <button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
-      <p>Not a member? <a href="#">Sign Up</a></p>
+      <p>Not a member? <a href="register">Sign Up</a></p>
+
     </div>
+
   </div>
 
   </div>
+  </div>
+
+  <div>
+      @include('common.errors')
   </div>
