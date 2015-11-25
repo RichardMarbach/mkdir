157a60c9acec2f21e41d6cd45d3b0dac53660c1e
Richard Marbach <rmarbach@gmail.com>
2015-11-25 23:17:36 +0100

    dvd items now visible

--- resources/views/admin/partials/dvd-item.blade.php
+++ resources/views/admin/partials/dvd-item.blade.php
@@ -1,5 +1,5 @@
 <div class="col-sm-4">
-  <div class="thumbnail row" data-id="{{$dvd->id}}">
+  <div class="thumbnail" data-id="{{$dvd->id}}">
     <div class="col-sm-8">
       <h4 data-name="{{ $dvd->name }}"> {{ $dvd->title }} 
         <span data-length="{{ $dvd->length }}" class="pull-right">{{ $dvd->length }} min</span>
@@ -9,6 +9,6 @@
     <div class="col-sm-4" data-cover="{{ $dvd->cover_image }}">
       <img class="img-responsive" src="{{ $dvd->getCoverImage() }}" alt="">
     </div>
-    <h5 data-stock="{{ $dvd->totalStock() }}">{{ $dvd->stock() }} / {{ $dvd->totalStock() }}</h5>
+    <h5 data-stock="{{ $dvd->totalStock() }}">Stock: {{ $dvd->stock() }} / {{ $dvd->totalStock() }}</h5>
   </div>
 </div>
