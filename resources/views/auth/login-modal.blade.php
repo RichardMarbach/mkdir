705297692d03aaf8bf08a7042a4cad1d8f3f8ef8
Richard Marbach <rmarbach@gmail.com>
2015-11-26 11:23:08 +0100

    Fixed login modal form

--- resources/views/auth/login-modal.blade.php
+++ resources/views/auth/login-modal.blade.php
@@ -6,25 +6,32 @@
       <img src="images/img/mkdir_logo.png"
       style="width:100%;"/>
     </div>
+
     <div class="modal-body" style="padding:40px 50px;">
-      <form role="form">
+      {!! Form::open(['url' => 'login', 'method' => 'post', 'class' => 'form-horizontal' ,'role' => 'form']) !!}
+
         <div class="form-group">
-          <label for="usrname"><span class="glyphicon glyphicon-user"></span> Email</label>
-          <input type="text" class="form-control" id="usrname" placeholder="Enter email">
+          <label for="email"><span class="glyphicon glyphicon-user"></span> Email</label>
+          <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}" placeholder="Enter email">
         </div>
+
         <div class="form-group">
-          <label for="psw"><span class="glyphicon glyphicon-lock"></span> Password</label>
-          <input type="password" class="form-control" id="psw" placeholder="Enter password">
+          <label for="password"><span class="glyphicon glyphicon-lock"></span> Password</label>
+          <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
         </div>
+
         <div class="checkbox">
-          <label><input type="checkbox" value="" checked>Remember me</label>
+          <label><input type="checkbox" checked>Remember me</label>
         </div>
           <button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Login</button>
-      </form>
+      {!! Form::close() !!}
     </div>
+
     <div class="modal-footer">
-      <button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
-      <p>Not a member? <a href="register">Sign Up</a></p>
+      <button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal">
+        <span class="glyphicon glyphicon-remove"></span> Cancel
+      </button>
+      <p>Not a member? <a href="{{ url('register') }}">Sign Up</a></p>
 
     </div>
 
